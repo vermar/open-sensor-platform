83c873eba5945dd2005af1b43df552214b056e6b
Rajiv Verma <vermar@users.noreply.github.com>
2024-07-03 14:45:19 -0700

    Updated to reflect USB host library changes

--- embedded/projects/Discovery_F4_MP3/Sources/Config/usbh_conf.h
+++ embedded/projects/Discovery_F4_MP3/Sources/Config/usbh_conf.h
@@ -2,32 +2,22 @@
   ******************************************************************************
   * @file    FatFs/FatFs_USBDisk/Inc/usbh_conf.h
   * @author  MCD Application Team
-  * @version V1.3.2
-  * @date    13-November-2015
   * @brief   General low level driver configuration
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
+  * Copyright (c) 2017 STMicroelectronics.
+  * All rights reserved.
   *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
-
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __USBH_CONF_H
-#define __USBH_CONF_H
+#ifndef __USBH_CONF__H__
+#define __USBH_CONF__H__
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
@@ -48,7 +38,7 @@
 
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
-/* CMSIS OS macros */   
+/* CMSIS OS macros */
 #if (USBH_USE_OS == 1)
 # ifndef __CMSIS_RTOS
 #  include "common.h"
@@ -57,41 +47,41 @@
 # define   USBH_PROCESS_PRIO          osPriorityNormal
 # define   USBH_PROCESS_STACK_SIZE    (8 * 128)
 # endif
-#endif  
+#endif
 
-/* Memory management macros */   
+/* Memory management macros */
 #define USBH_malloc               malloc
 #define USBH_free(x)              free(x)
 #define USBH_memset               memset
 #define USBH_memcpy               memcpy
-    
- /* DEBUG macros */  
+
+ /* DEBUG macros */
 #if (USBH_DEBUG_LEVEL > 0)
 #define  USBH_UsrLog(...)   D1_printf(__VA_ARGS__);\
                             D1_printf("\n");
 #else
-#define USBH_UsrLog(...)   
-#endif 
-                            
-                            
+#define USBH_UsrLog(...)
+#endif
+
+
 #if (USBH_DEBUG_LEVEL > 1)
 
 #define  USBH_ErrLog(...)   D1_printf("ERROR: ") ;\
                             D1_printf(__VA_ARGS__);\
                             D1_printf("\n");
 #else
-#define USBH_ErrLog(...)   
-#endif 
-                            
-#if (USBH_DEBUG_LEVEL > 2)                         
+#define USBH_ErrLog(...)
+#endif
+
+#if (USBH_DEBUG_LEVEL > 2)
 #define  USBH_DbgLog(...)   D1_printf("DEBUG : ") ;\
                             D1_printf(__VA_ARGS__);\
                             D1_printf("\n");
 #else
-#define USBH_DbgLog(...)                         
+#define USBH_DbgLog(...)
 #endif
-                            
-                              
+
+
 #if (USBH_MAX_NUM_CONFIGURATION > 1)
 #error This USB Host Library version Supports only 1 configuration!
 #endif
@@ -99,7 +89,4 @@
 /* Exported functions ------------------------------------------------------- */
 void ASFMessagePut( TaskId tid, uint32_t info, uint32_t timeout );
 
-#endif /* __USB_CONF_H */
-    
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
+#endif /* __USBH_CONF__H__ */
